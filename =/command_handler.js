3742046c66dbcdc7b903e224ec8716c050717426
Thrillonek <jindrich.kraina@gmail.com>
2022-03-14 16:54:52 +0100

    g

--- =/command_handler.js
+++ =/command_handler.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 
 module.exports = (client, discord) =>{
-    const single_files = fs.readdirSync(`./Commands`).filter(file => file.endsWith('.js'));
+    const single_files = fs.readdirSync(`./commands`).filter(file => file.endsWith('.js'));
 
     for(const single_file of single_files){
-        const command = require(`../Commands/${single_file}`);
+        const command = require(`../commands/${single_file}`);
         if(command.name){
             client.commands.set(command.name, command);
         } else {
@@ -13,10 +13,10 @@ module.exports = (client, discord) =>{
     }
 
     const load_dir = (dirs) =>{
-      const files = fs.readdirSync(`./Commands/${dirs}/`).filter(file => file.endsWith('.js'));
+      const files = fs.readdirSync(`./commands/${dirs}/`).filter(file => file.endsWith('.js'));
 
       for(const file of files){
-          const command = require(`../Commands/${dirs}/${file}`);
+          const command = require(`../commands/${dirs}/${file}`);
           if(command.name){
               client.commands.set(command.name, command);
           } else {
